f6ccedcccac24c28410bdb2f4254c4584fe3654b
Kanashin_Dmitry <dmitrii.a.kanashin@gmail.com>
2020-03-16 01:13:21 +0300

    Added assignment_2

--- assignment_2/server/app_db.py
+++ assignment_2/server/app_db.py
@@ -1,8 +1,11 @@
 import json
 import sqlite3
+import requests
 
 from flask import Flask, g, request
 from flask_cors import CORS
+from bs4 import BeautifulSoup
+from requests.exceptions import HTTPError
 
 app = Flask(__name__)
 CORS(app)
@@ -22,24 +25,60 @@ def init_db():
         db = get_db()
         cursor = db.cursor()
         cursor.executescript(
-            """CREATE TABLE IF NOT EXISTS Users
-               (id integer primary key, name text not null,
-               surname text not null, age integer)"""
+            """CREATE TABLE IF NOT EXISTS Rankings
+               (position integer primary key
+               , team text not null
+               , logo text not null
+               , players text not null
+               , diff text not null
+               , points text not null)"""
         )
+        parse_html("https://www.hltv.org/ranking/teams")
+        
         db.commit()
+
+        
+def request_url(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+    else:
+        return response.content
+
+    
+def parse_html(url):
+    page_cont = request_url(url)
+    soup = BeautifulSoup(page_cont, 'html.parser')
+    for team in soup.find_all('div', class_="ranked-team"):
+        team_name = team.find('span', class_="name").text
+        logo = team.find('img')['src']
+        position = team.find('span', class_="position").text
+        players = ', '.join([player.text for player in team.find_all('div', class_="rankingNicknames")])
+        points = team.find('span', class_="points").text
+        diff = team.find('div', {"class":["change positive", "change negative"]}) #', '.join([player.text for player in team.find_all('div', class_="rankingNicknames")])
+        diff = "no difference" if diff is None else diff.text
+        cursor = get_db().cursor()
+        cursor.execute('''  INSERT OR REPLACE INTO Rankings(position, team, logo, players, diff, points) 
+                            VALUES(?, ?, ?, ?, ?, ?)''', 
+                       (int(position[1:]), team_name, logo, players, diff, points[1:-1]))
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
 
 @app.route('/get_all')
 def get_all():
     db_cursor = get_db().cursor()
     db_cursor.row_factory = sqlite3.Row
-    db_cursor.execute("SELECT * From Users")
+    db_cursor.execute("SELECT * From Rankings")
     result = db_cursor.fetchall()
     json_result = json.dumps([dict(row) for row in result])
     return json_result
 
 
-@app.route('/new_user', methods=['POST'])
+'''@app.route('/new_user', methods=['POST'])
 def create_new_user():
     user_json = request.get_json()
     for key in ['name', 'surname', 'age']:
@@ -50,7 +89,7 @@ def create_new_user():
     db_conn.commit()
     db_conn.close()
     return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
-
+'''
 
 @app.teardown_appcontext
 def close_connection(exception):
