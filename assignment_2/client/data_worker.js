f3b9bbb2d2df5122a91593147ac6dddbb25d28b9
Kanashin_Dmitry <dmitrii.a.kanashin@gmail.com>
2020-03-16 00:55:41 +0300

    Added assignment_2

--- assignment_2/client/data_worker.js
+++ assignment_2/client/data_worker.js
@@ -7,51 +7,18 @@ function draw_table() {
     $.getJSON('http://127.0.0.1:5000/get_all', function (data) {
         $.each(data, function (key, val) {
             let row = "";
+            let team = "";
             $.each(val, function (key, val) {
-                row += '<td>' + val + '</td>';
+                if (key == 'team_name'){
+                    team = val;
+                }
+                if (key == 'logo'){
+                    row += '<td><img alt="' + team + '" src="' + val + '" width="100" height="100"></td>';
+                } else {
+                    row += '<td>' + val + '</td>';
+                }
             });
             $('#table_body').append('<tr>' + row + '</tr>');
         });
     });
-}
-
-
-function processForm(e) {
-    if (e.preventDefault) e.preventDefault();
-    let user_info = $('#form').serializeArray().reduce(function (obj, item) {
-        obj[item.name] = item.value;
-        return obj;
-    }, {});
-    let promise = new Promise(function(resolve, reject) {
-        $.ajax({
-            type: "POST",
-            contentType: "application/json; charset=utf-8",
-            url: "http://127.0.0.1:5000/new_user",
-            data: JSON.stringify(user_info),
-            dataType: "json",
-            success: function (data) {
-                resolve(data);
-            },
-            error: function (err) {
-                reject(err);
-            }
-        });
-    });
-
-    promise.then(function (_) {
-        draw_table();
-    }).catch(function (err) {
-        console.log("can't add new user: " + err);
-    })
-
-    // request.always(function (data) {
-    //     draw_table();
-    // });
-}
-
-const form = document.getElementById('form');
-if (form.attachEvent) {
-    form.attachEvent("submit", processForm);
-} else {
-    form.addEventListener("submit", processForm);
 }
